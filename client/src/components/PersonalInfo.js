5ccbc5fb07729550e2b2ee28e2e823f67e1b27fc
tduncan14 <nbatre1@yahoo.com>
2022-05-01 17:29:26 -0700

    keep moving forward

--- client/src/components/PersonalInfo.js
+++ client/src/components/PersonalInfo.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Form,Input} from 'antd'
 
 
+const {TextArea} = Input
+
+
 
 const PersonalInfo = () => {
 
@@ -29,7 +32,40 @@ const PersonalInfo = () => {
                              </Form.Item>
                          
                          </div>
+
+                         
+         
+                 <div className="col-md-4">
+                         <Form.Item name="Mobile Number" label="Mobile Number" rules={[{required:true}]}>
+                           <Input />
+                         </Form.Item>
+                     </div>
+                     <div className="col-md-4">
+                           <Form.Item name="portfolio" label="Portfolio" rules = {[{required:true}]}>
+                               <Input />
+                           </Form.Item>
+                         
+                         </div>
+                         <div className="col-md-4">
+                             
+                         </div>
+
+
+                         <div className="col-md-12">
+                         <Form.Item name="Career Objective" label="Career Objective" rules={[{required:true}]}>
+                           <TextArea />
+                         </Form.Item>
+                     </div>
+                     <div className="col-md-12">
+                           <Form.Item name="Address" label="Address" rules = {[{required:true}]}>
+                           <TextArea />
+                           </Form.Item>
+                         
+                         </div>
+
                  </div>
+
+                         
              </div>
 
 
